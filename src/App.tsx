d77fa89ad7448531b70d4ebaddcaecd722e4ce44
packirisamykaran <43819446+packirisamykaran@users.noreply.github.com>
2022-06-02 14:31:45 +0800

    github router fix 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 import Login from './components/auth/Login';
 import './App.css'
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import AppProvider from './contexts/AppProvider';
 import RequireLogin from "./components/hoc/RequireLogin"
 import Profile from './components/profile/Profile';
@@ -16,11 +16,11 @@ import UpdateProfile from './components/profile/UpdateProfile';
 function App() {
   
   return (
-    <HashRouter>
+    <BrowserRouter basename='/daily-thoughts'>
       <AppProvider>
       <div className="App">
             <Routes>
-              <Route path="/daily-thoughts" element={<RequireLogin><AppNav/></RequireLogin>} >
+              <Route path="/" element={<RequireLogin><AppNav/></RequireLogin>} >
                   <Route  path='profile/:username' element={<Profile/>} />
                   <Route path='explore' element={<Explore />} >
                     <Route path='profile/:username' element={<Profile/>} />
@@ -34,7 +34,7 @@ function App() {
             </Routes>
       </div>
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
